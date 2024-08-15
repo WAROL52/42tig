ffd4fef72b28d11fcc47b93c180bfff8ec0c8633
WAROL52 <rabetsyrolio@gmail.com>
2024-08-15 18:49:48 +0300

    <42tig>[Auto-push](55): Sauvegarde auto

--- utils/push_swap/run.sh
+++ utils/push_swap/run.sh
@@ -32,7 +32,7 @@ function vrun(){
 	echo "Temps: $timesDiff ms "
 	grep "in use at exit" < $tmp | sed 's/^==[0-9]*==     in use at exit/Memoire/'
 	grep "total heap usage" < $tmp | sed 's/^==[0-9]*==   total heap usage/Allocation/'
-	echo -e "${Normal}"local t2=$(date '+%s')
+	echo -e "${Normal}"
 }
 function infinityRun() {
 	local PID_SERVER=$1
@@ -52,7 +52,7 @@ function run() {
 	if [ $? -gt 0 ]; then
     	echo "Les chaînes sont identiques."
 	else
-		vrun ./push_swap $tmpVClient '6 5 2 9 4 8 3 7 1 10 '
+		vrun ./push_swap $tmpVClient '58 33 22 98 25 92 28 15 66 8 87 23 74 10 27 35 77 31 5 6 82 60 32 68 4 29 7 54 76 45 14 11 26 17 63 56 95 20 99 19 34 51 70 16 85 79 40 37 18 52 1 41 49 91 100 83 72 97 43 53 50 64 13 3 62 96 2 73 93 69 80 12 67 55 88 48 38 36 9 81 39 59 90 61 89 94 46 42 65 44 57 71 86 78 30 75 24 84 21 47 '
 		# vrun ./push_swap $tmpVClient '3 6 5' 2 4  +10
  		# vrun ./push_swap $tmpVClient 5 1 3 6 2 4
 	fi
