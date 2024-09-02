e077bf9ff0a8fda9f62237500bf411da60a84e32
WAROL52-zorin <rabetsyrolio@gmail.com>
2024-09-03 01:28:45 +0300

    [Auto-push 152]:

--- .tig/bash/git.sh
+++ .tig/bash/git.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
+
+git_push(){
+	local name=$1
+	printf "\033[0;34m|--------------------|\033[0m\n"
+	printf "|%s|\n" "$name"
+	printf "\033[0;34m|--------------------|\033[0m\n"
+	cd $name
+	pwd
+	if [ -e ".git" ]; then
+      # Vérifier s'il y a des modifications à committer
+      if [ -n "$(git status --porcelain)" ]; then
+        # Ajouter tous les changements
+        git add .
+        
+        # Effectuer un commit avec un message générique
+        git commit -m "Mise à jour automatique des projets"
+        
+        # Effectuer un push vers le dépôt distant
+        git push $(GIT_REMOTE) $(GIT_BRANCH) # Remplacez 'main' par la branche appropriée si nécessaire
+        
+        echo "Push effectué pour $name"
+      else
+        echo "Aucune modification à pousser pour $name"
+      fi
+    else
+      echo "Le répertoire $name n'est pas un dépôt Git."
+    fi
+	cd ..
+}
+
+
 git_push_auto() {
 
-# Parcourir chaque projet
-while read -r name url; do
-  project_dir="$REPOS_DIR/$name"
-  
-  if [ -d "$project_dir" ]; then
-    cd "$project_dir" || continue
-    pwd
-	if [ -d ".git" ]; then
-    continue
-    # Ajouter tous les changements
-    git add .
-    
-    # Effectuer un commit avec un message générique
-    git commit -m "Mise à jour automatique des projets"
-    
-    # Effectuer un push vers le dépôt distant
-    git push origin main # Remplacez 'main' par la branche appropriée si nécessaire
-    
-    echo "Push effectué pour $name"
-  else
-    echo "Le répertoire $name n'existe pas dans $REPOS_DIR"
-  fi
-done < <(echo "$GITHUB_URL" | awk '{for (i=1; i<=NF; i+=2) print $i, $(i+1)}')
+	# Parcourir chaque projet
+	cd $REPOS_DIR
+	while read -r name url; do
+	echo "pour $name"
+	pwd
+	git_push $name
+	echo -e "\n"
+	done < <(echo "$GITHUB_URL" | awk '{for (i=1; i<=NF; i+=2) print $i, $(i+1)}')
 }
