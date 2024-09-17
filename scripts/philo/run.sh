5666042dee4c1312d8db8ea9f9f3aecbc6981599
WAROL52 <rabetsyrolio@gmail.com>
2024-09-17 18:49:08 +0300

    mise a jour de tous

--- scripts/philo/run.sh
+++ scripts/philo/run.sh
@@ -25,19 +25,28 @@ function logVar(){
 	echo "
 	SCRIPT_DIR=$SCRIPT_DIR
 	WORKSPACE_DIR=$WORKSPACE_DIR
-	
 	CFLAGS=$CFLAGS
 	CFLAGSW=$CFLAGSW
 	CFLAGS_VALGRIND=$CFLAGS_VALGRIND
 	OUT_DIR=$OUT_DIR
 	TIG_DIR=$TIG_DIR
-	----------------
+	PROJET_NAME=$PROJET_NAME
+	# ----------------------------
 	WORKSPACE_PATH=$WORKSPACE_PATH
-
 	SCRIPT_PATH=$SCRIPT_PATH
+	OUT_PATH=$OUT_PATH
 	"
 }
-function make_clear() {
+function make_run() {
+	make_all
+
+	valgrind_run "$WORKSPACE_PATH/philo" 1  200 200
+	# make_re
+	# echo "A FAIRE..."
+	# logVar
+}
+
+function make_run_bonus() {
 	# valgrind_run <nom_du_fishier> [args...]
 	# make_run
 	# make_re
--- scripts/so_long/run.sh
+++ scripts/so_long/run.sh
@@ -5,9 +5,9 @@
 #-----------------------------
 # Pour aider ...
 # ----------------------------
-UTILS_DIR=$UTILS_DIR
-REPOS_DIR=$REPOS_DIR
-RUN_DIR=$RUN_DIR
+SCRIPT_DIR=$SCRIPT_DIR
+WORKSPACE_DIR=$WORKSPACE_DIR
+
 CFLAGS=$CFLAGS
 CFLAGSW=$CFLAGSW
 CFLAGS_VALGRIND=$CFLAGS_VALGRIND
@@ -16,15 +16,15 @@ TIG_DIR=$TIG_DIR
 PROJET_NAME=$PROJET_NAME
 # ----------------------------
 WORKSPACE_PATH=$WORKSPACE_PATH
-RUN_PATH=$RUN_PATH
-UTILS_PATH=$UTILS_PATH
+
+SCRIPT_PATH=$SCRIPT_PATH
 OUT_PATH=$OUT_PATH
 ##############################
 function logVar(){
 	echo "
-	UTILS_DIR=$UTILS_DIR
-	REPOS_DIR=$REPOS_DIR
-	RUN_DIR=$RUN_DIR
+	SCRIPT_DIR=$SCRIPT_DIR
+	WORKSPACE_DIR=$WORKSPACE_DIR
+	
 	CFLAGS=$CFLAGS
 	CFLAGSW=$CFLAGSW
 	CFLAGS_VALGRIND=$CFLAGS_VALGRIND
@@ -32,14 +32,14 @@ function logVar(){
 	TIG_DIR=$TIG_DIR
 	----------------
 	WORKSPACE_PATH=$WORKSPACE_PATH
-	RUN_PATH=$RUN_PATH
-	UTILS_PATH=$UTILS_PATH
+
+	SCRIPT_PATH=$SCRIPT_PATH
 	"
 }
 
 
 function make_run() {
-	local args=$(head -n 1 $UTILS_PATH/args.txt)
+	local args=$(head -n 1 $SCRIPT_PATH/args.txt)
 	# make_all
 	mkdir -p $OUT_PATH
 	cd $WORKSPACE_PATH
