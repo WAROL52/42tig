94c6bdb1a24005ffa03541a065806b0c0809de6d
WAROL52 <rabetsyrolio@gmail.com>
2024-10-11 06:29:54 +0300

    mise a jour de 42tig

---
+++ scripts/script/init.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+##############################
+# VARIABLE GLOBAL
+#-----------------------------
+# Pour aider ...
+# ----------------------------
+SCRIPT_DIR=$SCRIPT_DIR
+WORKSPACE_DIR=$WORKSPACE_DIR
+
+CFLAGS=$CFLAGS
+CFLAGSW=$CFLAGSW
+CFLAGS_VALGRIND=$CFLAGS_VALGRIND
+OUT_DIR=$OUT_DIR
+TIG_DIR=$TIG_DIR
+PROJET_NAME=$PROJET_NAME
+# ----------------------------
+WORKSPACE_PATH=$WORKSPACE_PATH
+
+SCRIPT_PATH=$SCRIPT_PATH
+OUT_PATH=$OUT_PATH
+##############################
+
+function logVar(){
+	echo "
+	SCRIPT_DIR=$SCRIPT_DIR
+	WORKSPACE_DIR=$WORKSPACE_DIR
+	
+	CFLAGS=$CFLAGS
+	CFLAGSW=$CFLAGSW
+	CFLAGS_VALGRIND=$CFLAGS_VALGRIND
+	OUT_DIR=$OUT_DIR
+	TIG_DIR=$TIG_DIR
+	----------------
+	WORKSPACE_PATH=$WORKSPACE_PATH
+
+	SCRIPT_PATH=$SCRIPT_PATH
+	"
+}
+
+function make_init() {
+	# valgrind_run <nom_du_fishier> [args...]
+	# make_run
+	# make_re
+	echo "A FAIRE..."
+	logVar
+}
